07844d08912ec63942689ca3d5e1fae3d94070f5
Nur fadhil aminullah <nurfadhil138@gmail.com>
2022-06-24 23:13:00 +0700

    micro-project

---
+++ src/views/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container">
+    <div class="row mb-4">
+        <div class="col-12">
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 100%; border:none"
+            class="mb-2"
+          >
+            <b-card-text>
+                    <a href="https://www.facebook.com/sharer.php?u=[/card]">
+                        <p>
+                        facebook
+                        </p>
+                    </a>
+            </b-card-text>
+            <b-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nisl ante, mollis eget porta quis, consequat et lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut non purus quam. Nullam ut condimentum nisl, eget blandit est. In hac habitasse platea dictumst. Donec leo elit, vulputate eu imperdiet vitae, feugiat eu odio. Vestibulum vehicula gravida dui, vel cursus dolor tincidunt in. Pellentesque sodales congue sem, sed suscipit erat dapibus quis. Nullam at quam fermentum, mattis mi quis, venenatis lorem. Pellentesque mollis massa rutrum lacinia hendrerit. In hac habitasse platea dictumst. Etiam nec tortor aliquam, sodales risus vel, dapibus nunc. Aenean massa orci, dignissim dignissim massa ac, vestibulum porta nisi. Maecenas porttitor ut quam vel bibendum.
+              
+            </b-card-text>
+
+          </b-card>
+        </div>
+      </div>
+  </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+// import CardProduct from "@/components/CardProduct.vue";
+// import axios from "axios";
+
+export default {
+  name: "Card",
+  components: {
+    // CardProduct,
+    Navbar,
+
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="container">
+      <Hero />
+
+      <div class="row mt-4">
+        <div class="col">
+          <h2>
+            Best
+            <strong>Foods</strong>
+          </h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right">
+            <b-icon-eye></b-icon-eye>Lihat Semua
+          </router-link>
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div cols="4" cols-sm="12">
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="/card" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </div>
+        <div cols="4" cols-sm="12">
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </div>
+        <div cols="4" cols-sm="12">
+          <b-card
+            title="Card Title"
+            img-src="https://picsum.photos/600/300/?image=25"
+            img-alt="Image"
+            img-top
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+// import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
+
+export default {
+  name: "Home",
+  components: {
+    Navbar,
+    Hero,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then((response) => this.setProducts(response.data))
+      .catch((error) => console.log(error))
+  },
+};
+</script>
